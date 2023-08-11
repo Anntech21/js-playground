37501f9b4b1053004e59ca64fe53e513dc36dfba
Ann Tech <anntech21@gmail.com>
2023-08-11 10:40:44 -0700

    arrays

---
+++ arrays.js
@@ -0,0 +1,16 @@
+let color1 = "red";
+let color2 = "blue";
+let color3 = "yellow";
+let color4 = "green";
+
+let colors = ["red", "blue", "yellow", "green"];
+let evenNumbers = [4, 2, 6, 12, 1024];
+
+//let badPracticeMixedArray = ["Colin", 43, true];
+
+let books = ["Hackers", "Capitalism", "Unicorn"];
+//let colorsAgain = [color1, color2, color3, color4];
+let colorsAgain = [color1, color2.toUpperCase(), color3, color4];
+console.log(colorsAgain);
+
+let bookIHaventReadYet = books[1];
