628aa33d9f41607170d505937b0329c4207db56e
Ann Tech <anntech21@gmail.com>
2023-08-11 12:49:00 -0700

    arrays more

--- arrays.js
+++ arrays.js
@@ -27,16 +27,34 @@ let apple = books[0];
 let banana = apple[1];
 let mango = books[0].slice(0, 4).toUpperCase();
 
-
 let myBookCollection = ["Uni, Invisibles", ["Mockingjay", "Catching Fire "]];
 //myBookCollection [2][2]; arrays in arrays answer "Catching Fire"
 
 // .length is always the number of items in that array
 // you can evaluate something INSIDE the square brackets
-myBookCollection[15 -14]; 
+myBookCollection[15 - 14];
 books[evenNumbers[1]];
-books[books.length -1 ];// length - last item which will console as unicorn
+books[books.length - 1]; // length - last item which will console as unicorn
 
-//we changing the value ITSELF in array not reassigning. 
-//we have new renters in the house example, same house, same array, same variable,just changing the value
-colors[1] = 'fuschia'; //in console it will look like ['red', 'fuschia', 'yellow', 'green']
+//we changing the value ITSELF in array not reassigning.
+//we have new renters in the house example, same house, same array, same variable,just changing mutating the value
+colors[1] = "fuschia"; //in console it will look like ['red', 'fuschia', 'yellow', 'green']
+
+let bookHighlights = books[0] + " " + "and" + " " + books[1];
+//array item[variable] + "string" + array item[variable];
+
+colors.push("lavender");
+//console shows 5 values now adds at last
+colors.push("gold", "cyan", "goldenrod");
+
+colors.pop();
+//removes last
+colors.pop();
+
+colors.shift();
+colors.unshift("maroon", "purplr", "orange", "teal");
+//(9) ['maroon', 'purplr', 'orange', 'teal', 'fuschia', 'yellow', 'green', 'lavender', 'gold']
+
+let firstName ="Colin"
+firstName = firstNmae.toUpperCase();
+
